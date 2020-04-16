fa27843fa1ceb7cc16ac1bc75f029bacbbeee2d5
cpChen <305983806@qq.com>
2020-04-16 09:17:42 +0800

    日志

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import(
-	"github.com/Gryffindor-CN/simpel-printer/"
+import (
+	"./common"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	var bootstrap Bootstrap = new(LanCable)
+	common.Log.WithFields(logrus.Fields{
+		"animal": "monkey",
+		"size": 10,
+	}).Error("A group of walrus emerges from the ocean")
+	common.Log.WithFields(logrus.Fields{
+		"name": "cp",
+	}).Warn("hello world.")
+	common.Log.Info("info test.")
 }
+
+func init()  {
+	common.InitLog()
+}
+
+
+
+
+
