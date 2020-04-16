7ef56af49936c78d97893710d6df30bd3cb6f3c6
cpChen <305983806@qq.com>
2020-04-16 10:41:19 +0800

    添加新日志

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"./service"
 	"./common"
-	"github.com/sirupsen/logrus"
+	"./service"
+	//"github.com/sirupsen/logrus"
 )
 
 func main() {
-	common.Log.WithFields(logrus.Fields{
-		"animal": "monkey",
-		"size": 10,
-	}).Error("A group of walrus emerges from the ocean")
-	common.Log.WithFields(logrus.Fields{
-		"name": "cp",
-	}).Warn("hello world.")
-	common.Log.Info("info test.")
+	// 日志
+	//common.Log.WithFields(logrus.Fields{
+	//	"animal": "monkey",
+	//	"size": 10,
+	//}).Error("A group of walrus emerges from the ocean")
+	//common.Log.Info("info test.")
+
 	var bootStrap service.Bootstrap = new (service.LanCable)
 	bootStrap.Start()
 }
